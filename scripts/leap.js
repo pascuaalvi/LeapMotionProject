fe3fa104ec9c976d4589bf7f7990115a9b77db68
samuraiRAZR <alvinpascual21@ymail.com>
2015-08-09 19:19:19 +1200

    Inserted code from branch main-canvas. Also refactored out all the cats in the code.

--- scripts/leap.js
+++ scripts/leap.js
@@ -1,7 +1,7 @@
-var selected = 'images/circle.png';
-var catno = 2;
+var imgno = 2;
+$ = jQuery;
 
-var cats = {};
+var images = {};
 var options = { enableGestures: true };
 var last_rotate = 0;
 var reset = 1;
@@ -9,59 +9,79 @@ var rotation = 0;
 
 Leap.loop(options,function (frame) {
     frame.hands.forEach(function (hand, index) {
-        var cat = cats[index] || (cats[index] = new Cat(selected));
+        var images = images[index] || (images[index] = new Image(selected));
 
         if (hand.grabStrength>0.7) {
-            // replace this code by whatever cat that is picked up
+            // replace this code by whatever image that is picked up
            if (reset==1) {
-                document.getElementById("Cat").src='images/images'+catno+'.jpg';
-                cat.setTransform(hand.screenPosition(), last_rotate);
+                document.getElementById("Image").src='images/images'+imgno+'.jpg';
+                image.setTransform(hand.screenPosition(), last_rotate);
                 reset = 0;
            }
         } else {
             reset = 1;
-            document.getElementById("Cat").src='images/circle.png';
-            cat.setTransform(hand.screenPosition(), last_rotate);
+            document.getElementById("Image").src='images/circle.png';
+            image.setTransform(hand.screenPosition(), last_rotate);
         }
         
         if (hand.grabStrength<0.8) {
-            cat.setTransform(hand.screenPosition(), rotation);
+            image.setTransform(hand.screenPosition(), rotation);
         } else {
             roll = hand.roll();
             if (roll>0.15 || roll<0.15) {
                 rotation = roll;
-                cat.setTransform(hand.screenPosition(), rotation);
+                image.setTransform(hand.screenPosition(), rotation);
             }
         }
     });
+    // Grab an Image
+    frame.gestures.forEach(function(gesture){
+      if (gesture.type === circle){
+        var image = grabbedHands[gesture.handIds[0]];
+        if (image == null){
+          //ignore
+        } else {
+          // apply image scaling
+        }
+      }
+    });
 }).use('screenPosition', { scale: 1 });
 
+var Image = function(imgNo) {
+  var image = this;
+  var img = document.createElement('img');
+  img.id = "Image"+imgNo;
+  img.src = 'images/images'+imgNo+'.jpg';
+  img.style.position = 'inline-block';
+  img.style.width = '200px';
+  img.style.height = '25%';
+  img.onload = function () {
+    //image.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
+    //document.body.appendChild(img);
+    $("#thumbnail").append(img);
+  }
 
+  image.setAsCurrentImage = function(){
+    $("#thumbnail img").removeClass("selected");
+    img.classList.add("selected");
+  }
+  
+  image.setTransform = function(position, rotation) {
+    if($(this).hasClass("selected")){
+      console.log(position+" X "+rotation);
+      img.style.left = position[0] - img.width  / 2 + 'px';
+      img.style.top  = position[1] - img.height / 2 + 'px';
 
-var Cat = function (image) {
-    var cat = this;
-    var img = document.createElement('img');
-    img.id = "Cat"
-    img.src = image;
-    img.style.position = 'absolute';
-
-    img.onload = function () {
-        cat.setTransform([0,0], 0);
-        document.body.appendChild(img);
-    };
-    
-    cat.setTransform = function (position, rotation) {
-        img.style.left = position[0] - img.width / 2 + 'px' ;
-        img.style.top = window.innerHeight + position[1] - img.height /2 + 'px';
-        img.style.transform = 'rotate(' + -rotation + 'rad)';
-        img.style.webkitTransform = img.style.MozTransform = img.style.msTransform = img.style.OTransform = img.style.transform;
-
-    };
-    
-    
-    
+      img.style.transform = 'rotate(' + -rotation + 'rad)';
+      
+      img.style.webkitTransform = img.style.MozTransform = img.style.msTransform =
+      img.style.OTransform = img.style.transform;
+    }
+  };   
 };
 
-cats[0] = new Cat(selected);
+for(var i = 0 ; i < 5 ; i++){
+  images[i] = new Image(i+1);
+}
 
 Leap.loopController.setBackground(true);
